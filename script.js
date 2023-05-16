5e181942ea2852f157683e29c825c86e3f19aad9
erik-r-u-kodehode <125983659+erik-r-u-kodehode@users.noreply.github.com>
2023-05-16 13:07:02 +0200

    Added active state for numpad keypress.

--- script.js
+++ script.js
@@ -3,28 +3,35 @@ const display = document.getElementById("display");
 const volumeSlider = document.getElementById("volume-slider");
 const soundFolder = "sounds/"
 const soundList = [
-  {key: "1", fileName: "snap.wav"},
-  {key: "2", fileName: "tink.wav"},
-  {key: "3", fileName: "tom.wav"},
+  {key: "7", fileName: "snap.wav"},
+  {key: "8", fileName: "tink.wav"},
+  {key: "9", fileName: "tom.wav"},
   {key: "4", fileName: "kick.wav"},
   {key: "5", fileName: "lohat.wav"},
   {key: "6", fileName: "ride.wav"},
-  {key: "7", fileName: "snare.wav"},
-  {key: "8", fileName: "clap.wav"},
-  {key: "9", fileName: "hihat.wav"},
+  {key: "1", fileName: "snare.wav"},
+  {key: "2", fileName: "clap.wav"},
+  {key: "3", fileName: "hihat.wav"},
 ]
 
 function drum(folder, fileInfo) {
 
   const button = document.createElement("button");
+  button.id = fileInfo.fileName;
   display.textContent = "";
 
   const sound = new Audio(folder + fileInfo.fileName)
 
   button.addEventListener("mousedown", () =>  {
     display.textContent = fileInfo.fileName.replace(/\.[^.]+$/, "");
+    button.classList.add("active");
     playSound(sound);
   })
+
+  button.addEventListener("mouseup", () => {
+    button.classList.remove("active");
+    button.blur();
+  });
 
   return button
 } 
@@ -34,14 +41,17 @@ const drumkitElements = soundList.map((soundInfo) => drum(soundFolder, soundInfo
 drumkitContainer.append(...drumkitElements)
 
 window.addEventListener("keydown", (event) => {
-
   const soundFile = soundList.find((soundInfo) => soundInfo.key === event.key);
-
 
   if (!soundFile) return;
 
-  const sound = new Audio(soundFolder + soundFile.fileName);
-  playSound(sound);
+  const elementToClick = document.getElementById(soundFile.fileName);
+  if (elementToClick) {
+    elementToClick.dispatchEvent(new Event("mousedown"));
+    setTimeout(() => {
+      elementToClick.dispatchEvent(new Event("mouseup"));
+    }, 60);
+  }
 });
 
 function playSound(audioElement) {
